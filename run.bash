7cecb3fa5aa78e64db09701f70e65521a4926f2c
hosea-imbo1 <83222304+hosea-imbo1@users.noreply.github.com>
2023-05-29 17:39:41 +0200

    Update run.bash

--- run.bash
+++ run.bash
@@ -5,3 +5,4 @@ git clone https://gitlab.dei.unipd.it/anemos/scaling-eureka.git
 cd /home/user/scaling-eureka/tb/bgp4/
 wget https://lxd.s3.eu-south-1.amazonaws.com/frr.tar.gz
 python3 bgp4.py TB00cefefefefe
+hosea
