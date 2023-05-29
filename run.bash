aa80723be02c2e7d9434e94d5e50a75319370f41
Fattokh Sadriddinov <fattokh.s@gmail.com>
2023-05-29 16:46:38 +0200

    Update run.bash

--- run.bash
+++ run.bash
@@ -4,4 +4,4 @@ https://vlabbooking.vdi.ict.unipd.it/
 git clone https://gitlab.dei.unipd.it/anemos/scaling-eureka.git
 cd /home/user/scaling-eureka/tb/bgp4/
 wget https://lxd.s3.eu-south-1.amazonaws.com/frr.tar.gz
-python3 bgp4.py TB00
+python3 bgp4.py TB00cefefefefe
